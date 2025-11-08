5cb0c0096bfff59419362d95a50bf73f4fa05a03
stevegiancopy-droid <stevegiancopy@gmail.com>
2025-11-08 21:05:33 +0200

    Update netlify-identity-widget.js

--- netlify-identity-widget.js
+++ netlify-identity-widget.js
@@ -1,12 +1,17 @@
 <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
 <script>
   if (window.netlifyIdentity) {
-    window.netlifyIdentity.on("init", user => {
+    window.netlifyIdentity.on("init", (user) => {
       if (!user) {
         window.netlifyIdentity.on("login", () => {
           document.location.href = "/admin/";
         });
       }
+    });
+
+    // Handle password recovery correctly
+    window.netlifyIdentity.on("recover", () => {
+      window.netlifyIdentity.open();
     });
   }
 </script>
